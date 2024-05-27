0b09857e087ec82d710c635d4be5cda9277a54d9
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-05-27 22:17:49 +0200

    added build tools

---
+++ test/test.asm
@@ -0,0 +1,21 @@
+#include "../header.asm"
+
+title:
+    .db "test float",0
+
+main:
+    bcall(_clrscrf)
+    bcall(_homeup)
+    ld hl, title
+    bcall(_puts)
+    bcall(_newline)
+
+    ld hl, test_data
+    bcall(_puts)
+    bcall(_newline)
+
+    bcall(_getkey) ; Pause
+    ret
+
+test_data:
+    .db "yrokpstylc",0
