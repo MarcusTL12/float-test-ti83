0e6625641d821e33a98bc983546557b1227e00c3
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-05-29 00:03:51 +0200

    started sdot

---
+++ blas/level1/sdot.asm
@@ -0,0 +1,19 @@
+#ifndef included_saxpy
+#define included_saxpy
+
+#include "z80float_brass/f32/f32mul.z80"
+
+; Inputs:
+;   BC: n
+;   HL: points to x
+;   DE: points to y
+;   IX: increment x
+;   IY: increment y
+;   stack 0: points to dest
+sdot:
+
+
+
+    ret
+
+#endif
