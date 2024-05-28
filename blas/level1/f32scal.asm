0011cfca2ce54a973fda5f5961476c15905e430c
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-05-28 11:52:56 +0200

    scal

---
+++ blas/level1/f32scal.asm
@@ -0,0 +1,51 @@
+#ifndef included_f32scal
+#define included_f32scal
+
+#include "z80float_brass/f32/f32mul.z80"
+
+; Inputs:
+;   BC: n
+;   HL: points to scalar
+;   DE: points to vector
+;   IX: increment
+f32scal:
+    push hl ; {0} Save pointer to scalar
+
+    ; mul increment by 4 (sizeof f32)
+    push ix ; {1}
+    pop hl ; {1}
+    add hl, hl
+    add hl, hl
+    push hl ; {1}
+    pop ix ; {1}
+    pop hl ; {0} restore scalar
+
+-:
+    push bc ; {0}
+    push ix ; {1} Save increment on stack
+
+    ; Perform mul
+    push de ; {2}
+    pop bc ; {2}
+    call f32mul
+
+    ; Increment de with increment
+    ex (sp), hl ; swap increment (on stack) with scalar
+    ex de, hl
+    add hl, de
+    ex de, hl
+    ex (sp), hl ; swap back
+
+    ; Loop logic
+    pop ix ; {1} restore increment
+    pop bc ; {0}
+    xor a
+    dec bc
+    or b
+    jr nz, {-}
+    or c
+    jr nz, {-}
+
+    ret
+
+#endif
--- test/test.asm
+++ test/test.asm
@@ -10,12 +10,62 @@ main:
     bcall(_puts)
     bcall(_newline)
 
-    ld hl, test_float1
-    ld de, test_float2
+    ld hl, main
+    bcall(_disphl)
+    bcall(_newline)
+    bcall(_getkey) ; Pause
+
+    ld hl, test_str1
+    ld bc, test_float1
+    call atof32
+
+    ld hl, test_str2
+    ld bc, test_float2
+    call atof32
+
+    ld hl, test_str3
     ld bc, test_float3
-    call f32pow
+    call atof32
+
+    ld hl, test_str4
+    ld bc, test_float4
+    call atof32
+
+    ld hl, test_str5
+    ld bc, test_float5
+    call atof32
+
+    ld bc, 4
+    ld hl, test_float5
+    ld de, test_float1
+    ld ix, 1
+    call f32scal
+
+    ld hl, test_float1
+    ld bc, test_data
+    call f32toa
+
+    ld hl, test_data
+    bcall(_puts)
+    bcall(_newline)
+
+    ld hl, test_float2
+    ld bc, test_data
+    call f32toa
+
+    ld hl, test_data
+    bcall(_puts)
+    bcall(_newline)
 
     ld hl, test_float3
+    ld bc, test_data
+    call f32toa
+
+    ld hl, test_data
+    bcall(_puts)
+    bcall(_newline)
+
+    ld hl, test_float4
     ld bc, test_data
     call f32toa
 
@@ -29,17 +79,40 @@ main:
 test_data:
     .db "xxxxxxxxxxxxxxxxxxxx",0
 
+test_str1:
+    .db "3.141592",0
+
+test_str2:
+    .db "5.789e-3",0
+
+test_str3:
+    .db "2.879e2",0
+
+test_str4:
+    .db "1998",0
+
+test_str5:
+    .db "2.0",0
+
 test_float1:
-    .dw e122h,301eh
+    .dw 0000h,0000h
 
 test_float2:
-    .dw 0e56h,4049h
+    .dw 0000h,0000h
 
 test_float3:
+    .dw 0000h,0000h
+
+test_float4:
+    .dw 0000h,0000h
+
+test_float5:
     .dw 0000h,0000h
 
 #define scrap saferam4
 #define char_NEG '-'
 
+#include "z80float_brass/conversion/atof32.z80"
 #include "z80float_brass/conversion/f32toa.z80"
-#include "z80float_brass/f32/f32pow.z80"
+
+#include "blas/level1/f32scal.asm"
