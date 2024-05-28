9672121c683f0c60757cb513fc51666016afabd2
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-05-28 09:40:12 +0200

    Translating labels from spasm to brass

--- convert_z80float.py
+++ convert_z80float.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 
 
 def replace_root_dir(dir):
@@ -12,7 +13,7 @@ def replace_root_dir(dir):
 dirs_to_convert = [
     "z80float/common/",
     "z80float/conversion/",
-    "z80float/f32",
+    "z80float/f32/",
 ]
 
 file_dict = {}
@@ -25,6 +26,8 @@ for dir in dirs_to_convert:
 
 
 include_reg = re.compile(r"#include\s+\"(.+)\"\s*")
+use_label_reg = re.compile(r"(\+|-)_")
+def_label_reg = re.compile(r"_:")
 
 
 def convert_file(filepath):
@@ -37,7 +40,7 @@ def convert_file(filepath):
     with open(filepath) as infile:
         lines = infile.readlines()
 
-    last_label = 0
+    have_labels_pointing_forward = False
 
     for i in range(len(lines)):
         line = lines[i]
@@ -46,15 +49,33 @@ def convert_file(filepath):
         if m is not None:
             if m.group(1) in file_dict:
                 lines[i] = f"#include \"{file_dict[m.group(1)]}\"\n"
+            continue
 
         if line.startswith("_:"):
-            pass
+            if have_labels_pointing_forward:
+                have_labels_pointing_forward = False
+                lines[i] = def_label_reg.sub("+:", line)
+            else:
+                lines[i] = def_label_reg.sub("-:", line)
+            continue
+
+        m = use_label_reg.search(line)
+
+        if m is not None:
+            if m.group(1) == "+":
+                have_labels_pointing_forward = True
+
+            lines[i] = use_label_reg.sub(r"{\1}", line)
 
     with open(relfile, "w") as outfile:
         outfile.writelines(lines)
 
 
+shutil.rmtree("z80float_relative")
+
+
 for dir in dirs_to_convert:
     for root, _, files in os.walk(dir):
         for file in files:
-            convert_file(f"{root}/{file}")
+            if file.endswith(".z80"):
+                convert_file(f"{root}/{file}")
